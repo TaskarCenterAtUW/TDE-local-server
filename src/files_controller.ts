4497bb57954109be2ed7abb5a37098a770103973
Naresh Kumar D <susrisha@gmail.com>
2022-12-15 12:30:38 +0530

    added file storage
    File storage listing is added.

--- src/files_controller.ts
+++ src/files_controller.ts
@@ -2,8 +2,11 @@ import { Request, Response, Router } from "express";
 import { UploadedFile } from "express-fileupload";
 // import { fstat } from "fs";
 import * as fs from 'fs';
+// import * as mime from "mime";
 import { FileEntity } from "./file_entity";
 // import {fs} from 'fs';
+import mime = require('mime-types');
+import path = require("path");
 
 class FilesController {
 
@@ -30,12 +33,8 @@ class FilesController {
     listFiles = async (request:Request, response: Response) => {
 
         const directoryPath = this.rootDir+request.params.path;
-        var filesList:FileEntity[] = [];
-        fs.readdirSync(directoryPath,{withFileTypes:true}).forEach(file => {
-            console.log(file.name);
-            console.log(file.isFile());
-            filesList.push(new FileEntity(file.name,request.params.path+file.name,file.isDirectory()));
-        });
+        var filesList:FileEntity[] = this.listFilesRecursively(directoryPath) ;//[];
+        
         response.status(200).send(filesList);
     }
 
@@ -60,6 +59,40 @@ class FilesController {
         response.status(200).send(request.params.path);
 
     }
+
+    // utility function
+
+private listFilesRecursively(dir: string): FileEntity[] {
+  // Create an empty array to store the list of files
+  const files: FileEntity[] = [];
+
+  // Read the contents of the directory
+  const entries = fs.readdirSync(dir, { withFileTypes: true });
+
+  // Iterate over the entries
+  for (const entry of entries) {
+    // Compute the full path of the entry
+    const entryPath = path.join(dir, entry.name);
+
+    // If the entry is a directory, list its files recursively
+    if (entry.isDirectory()) {
+      files.push(...this.listFilesRecursively(entryPath));
+    } else {
+      // Otherwise, the entry is a file, so add it to the list
+        const mimeType = mime.lookup(entry.name) as string;
+        const index = entryPath.indexOf(this.rootDir)+this.rootDir.length;
+        const relativePath = entryPath.substring(index);
+        const fileEntry =  new FileEntity(entry.name,relativePath,entry.isDirectory(),mimeType);
+        console.log(fileEntry);
+        
+      files.push(fileEntry);
+    }
+  }
+
+  // Return the list of files
+  return files;
+}
+
 }
 
 export default FilesController;
